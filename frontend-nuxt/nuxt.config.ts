f203e375725e6529874132c853ac2258f94f93f4
William Westwood <wwstwd@gmail.com>
2024-11-12 05:42:58 +0000

    removed previous frontend development

--- frontend-nuxt/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon'],
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in', duration: 500 },
-  },
-});
--- frontend-react/src/types/article.type.ts
+++
@@ -1,9 +0,0 @@
-export interface Article {
-  id?: string;
-  title: string;
-  body?: string[];
-  authors: string[];
-  categories: string[];
-  image_url?: string;
-  similarity?: number;
-}
